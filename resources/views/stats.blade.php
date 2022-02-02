9197263f0d44b89d27998b19161a1833fa3ac6fb
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-02 11:27:16 +0100

    .blade.php "automatizálás"

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>Museum bragging page</title>
-</head>
-<body>
-    <h1>Welcome to the Museum home page</h1>
-    <p>Total number of paintings: {{ $paintingCount }}</p>
-    <p>Total number of statues: {{ $statueCount }}, total price: {{ $statuePrice }} Ft</p>
-    <p>
-        <a href="{{ route('statues.index') }}">Go to statue listing</a>
-    </p>
-</body>
-</html>
+@extends('layout')
+
+@section('title', 'Museum bragging page')
+
+
+@section('content')
+    <div id="alap">
+        <h1>Welcome to the Museum home page</h1>
+        <p>Total number of paintings: {{ $paintingCount }}</p>
+        <p>Total number of statues: {{ $statueCount }}, total price: {{ $statuePrice }} Ft</p>
+        <p>
+            <a href="{{ route('statues.index') }}">Go to statue listing</a>
+        </p>
+    </div>
+@endsection
